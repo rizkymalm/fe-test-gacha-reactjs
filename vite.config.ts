31f6cfdf1128b0aacab67b9c1a9d28a93fda22d1
rizkymalm <rizkymalm@gmail.com>
2026-07-05 08:36:26 +0700

    fix: linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 /// <reference types="vitest/config" />
 // https://vite.dev/config/
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const dirname =
-    typeof __dirname !== 'undefined'
-        ? __dirname
-        : path.dirname(fileURLToPath(import.meta.url));
+// const dirname =
+//     typeof __dirname !== 'undefined'
+//         ? __dirname
+//         : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
